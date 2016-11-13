30dfe55f84115c1b1e31740cbdab1b1d1fdf8944
Justyna Dolińska <secansoida@users.noreply.github.com>
2016-11-13 16:53:23 +0100

    Simple octokit call

--- SimpleGHClient/ViewController.swift
+++ SimpleGHClient/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import OctoKit
+
+private let githubAPIURL = "https://api.github.com/v3";
 
 class ViewController: UIViewController {
 
+    private let client = OCTClient(server: OCTServer(baseURL: URL(string: githubAPIURL)))
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let request = client?.searchRepositories(withQuery: "SimpleGHClient", orderBy: nil, ascending: false)
+
+        let _ = request?.deliverOnMainThread().subscribeNext({ (output) in
+            guard let repos  = output as? OCTRepositoriesSearchResult else {
+                print("OUTPUT: \(output)")
+                return
+            }
+            print("REPOS COUNT: \(repos.repositories.count)")
+        }, error: { (error) in
+            if let error = error {
+                print("ERROR \(error)")
+            }
+        })
+
     }
 
     override func didReceiveMemoryWarning() {
